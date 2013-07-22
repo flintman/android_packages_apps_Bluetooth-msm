ebb75c6e76d998e26084c3952ef257d021273ee1
flintman <flintman@flintmancomputers.com>
2013-07-22 08:42:08 -0400

    Added Bluez ported from CAF

---
+++ Android.mk
@@ -0,0 +1,24 @@
+ifeq ($(BOARD_HAVE_BLUETOOTH_BLUEZ),true)
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := Bluetooth
+
+LOCAL_JAVA_LIBRARIES := javax.btobex
+LOCAL_JAVA_LIBRARIES += mms-common
+LOCAL_JAVA_LIBRARIES += telephony-common
+LOCAL_STATIC_JAVA_LIBRARIES := com.android.vcard
+
+LOCAL_CERTIFICATE := platform
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
---
+++ apptests/Android.mk
@@ -0,0 +1,4 @@
+
+LOCAL_PATH:= $(call my-dir)
+# uncomment to compile BT LE test apps
+#include $(call all-makefiles-under,$(LOCAL_PATH))
